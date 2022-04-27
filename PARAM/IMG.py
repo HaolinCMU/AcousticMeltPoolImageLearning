36878dfea44b5607434c9568c5da11db418ef661
HaolinCMU <haolinl@andrew.cmu.edu>
2022-04-27 07:46:44 -0400

    Add files via upload

---
+++ PARAM/IMG.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 28 01:19:34 2022
+
+@author: hlinl
+"""
+
+import os
+import numpy as np
+
+INTENSITY_THRESHOLD = (0.8, 1.0)
+
+# DBSCAN PARAM
+DBSCAN_EPSILON = 2
+DBSCAN_MIN_PTS = 5
+
+# PCA PARAM - one frame. 
+PC_NUM_FRAME = 2
+PCA_MODE_FRAME = 'transpose'
+FRAME_ALIGN_MODE = 'principal'
+FRAME_REALIGN_AXIS_VECT = np.array([0.,1.])
